67a5df424ce3172ad3659a4181caf9b2a9df4730
Hanzwellz <140514120+Hanzwellz@users.noreply.github.com>
2023-07-26 19:10:04 +0700

    Add files via upload

--- Assets/TutorialInfo/Scripts/Readme.cs
+++ Assets/TutorialInfo/Scripts/Readme.cs
@@ -1,12 +1,14 @@
-fileFormatVersion: 2
-guid: fcf7219bab7fe46a1ad266029b2fee19
-timeCreated: 1484669930
-licenseType: Store
-MonoImporter:
-  serializedVersion: 2
-  defaultReferences: []
-  executionOrder: 0
-  icon: {fileID: 2800000, guid: a186f8a87ca4f4d3aa864638ad5dfb65, type: 3}
-  userData: 
-  assetBundleName: 
-  assetBundleVariant: 
+﻿using System;
+using UnityEngine;
+
+public class Readme : ScriptableObject {
+	public Texture2D icon;
+	public string title;
+	public Section[] sections;
+	public bool loadedLayout;
+	
+	[Serializable]
+	public class Section {
+		public string heading, text, linkText, url;
+	}
+}
